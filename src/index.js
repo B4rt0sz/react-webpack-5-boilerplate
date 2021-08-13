0cd9e14413df44506f3f52b44a6437b24b1573b3
Bart <bartosz.szucko@gmail.com>
2021-08-13 23:58:59 +0200

    add babel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+import add from './add'
+
 const btn = document.querySelector('button')
 btn.addEventListener('click', () => console.log('click'))
+
+console.log(add(8, 21))
+
+document.querySelector('.text').innerHTML = 'JS works properly?'
