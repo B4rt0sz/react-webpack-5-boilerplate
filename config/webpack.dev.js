02bf6e4f6ca7a8735ca70c4e03cc859f6640d2f8
Bart <bartosz.szucko@gmail.com>
2021-08-14 18:25:08 +0200

    add HMR with react-refresh-webpack-plugin

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -9,12 +10,25 @@ module.exports = {
     historyApiFallback: true,
     contentBase: path.resolve(__dirname, '..', './dist'),
     open: true,
+    hot: true,
     compress: true,
     port: 8080,
   },
 
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              plugins: [require.resolve('react-refresh/babel')],
+            },
+          },
+        ],
+      },
       {
         test: /\.(s[ac]|c)ss$/i,
         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
@@ -26,5 +40,6 @@ module.exports = {
     new Dotenv({
       path: path.resolve(__dirname, '..', './.env.development'),
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
 }
