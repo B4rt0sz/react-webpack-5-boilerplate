fa9c2644cfc4c11306232fe1d53c26ff4cbc9a37
B4rt0sz <bart8.developer@gmail.com>
2023-05-06 16:58:36 +0200

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import logo from '../public/logo.svg'
 
 function App() {
   const text =
-    'Webpack 5 boilerplate with React 18 using Babel, HMR, Sass and PostCSS with a hot dev server and an optimized production build.'
+    'Webpack 5 boilerplate with React 18 using Babel 7, HMR, Sass and PostCSS with a hot dev server and an optimized production build.'
   const reminder =
     'Remember to add .env.development and .env.production to your .gitignore!'
   return (
